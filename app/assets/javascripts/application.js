a83645a1989b57c14fc8036c74e9670c99f08f5c
LooksLikeIveLost <ZeldaSpider599@gmail.com>
2017-07-26 17:38:08 +0000

    Installed jquery ui, sortable html5 library, and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require html.sortable
 //= require turbolinks
 //= require_tree .
 //= require jquery3
-
 //= require bootstrap-sprockets
